d0ee25a15cae60d2191145d0b836290a4dfae2b1
mutasim chaudhary <mutasim.chaud@gmail.com>
2023-11-30 18:04:13 +0000

    questions added to array

---
+++ index.js
@@ -0,0 +1,14 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+// TODO: Create an array of questions for user input
+const questions = ["What is your projects title?", "What is your desciption?", "What is your installation instructions?", "What is the usage information?", "What is the contribution Guidlines?","What are the test instruction?"];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
